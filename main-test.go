f53b4f5946ff67fb688070bb775651ce0da1a25b
Kevin Marschke <kmarschke@ellem.ai>
2019-11-15 15:31:29 -0600

    added work around logic

--- main-test.go
+++ main-test.go
@@ -1,4 +1,4 @@
-package test
+package main
 
 import "./package1"
 import "./package2"
@@ -7,5 +7,9 @@ func main() {
 	_ = package2.MyStruct {
 		MyField:package1.MyConst, // fails: cannot use package1.MyConst (constant 0 of type package1.MyInt) as package1.MyInt value in struct literal
 	}
+
+	mystruct := package2.MyStruct {}
+	package2.NotAWorkAround(&mystruct, package1.MyConst) // fails: cannot use package1.MyConst (constant 0 of type package1.MyInt) as package1.MyInt value in argument to package2.NotAWorkAround
+	package2.WorkAround(&mystruct, package1.MyConst) // works.
 }
 
--- package2/package2.go
+++ package2/package2.go
@@ -5,3 +5,16 @@ import "../package1"
 type MyStruct struct {
 	MyField package1.MyInt
 }
+
+// This work around removes the compiler's abilty to do any type-checking on myfield
+// thus will not result in the compile-time error.
+func WorkAround(mystruct *MyStruct, myfield interface{}) {
+	myfieldFromInterface,_ := myfield.(package1.MyInt)
+	mystruct.MyField        = myfieldFromInterface
+}
+
+
+// Trying to use this funciton in main will result in the same bug.
+func NotAWorkAround(mystruct *MyStruct, myfield package1.MyInt) {
+	mystruct.MyField = myfield
+}
